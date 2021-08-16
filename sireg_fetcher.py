fd168676036297e2be63f9c46840d063f6defba6
Ahmad Zafrullah <23pstars@gmail.com>
2021-08-16 15:57:06 +0800

    updates

--- sireg_fetcher.py
+++ sireg_fetcher.py
@@ -3,8 +3,8 @@
 import os.path
 import urllib
 
-src = '/home/zaf/Downloads/uangpangkal2020/m2.csv'
-dst = '/home/zaf/Downloads/uangpangkal2020/'
+src = '/home/zaf/Nextcloud/bank/bri/20211/d.csv'
+dst = '/home/zaf/Nextcloud/bank/bri/20211/foto/'
 
 with open(src) as _content:
     contents = _content.readlines()
@@ -14,10 +14,9 @@ photos = [i.strip().replace('"', '') for i in contents]
 for _user in photos:
     __user = _user.split(',')
     __photo = __user[1].split('.')
-    if len(__photo) >= 4:
-        __dest = dst + __user[0] + '.' + __photo[4]
-        if os.path.exists(__dest):
-            continue
-        urllib.urlretrieve(__user[1], __dest)
-        print(__user[1])
-        # print(dst + __user[0] + '.' + __photo[4])
+    __dest = dst + __user[0] + '.' + __photo[-1]
+    if os.path.exists(__dest):
+        continue
+    urllib.urlretrieve(__user[1], __dest)
+    print(__user[1])
+    # print(dst + __user[0] + '.' + __photo[-1])
