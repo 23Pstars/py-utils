3702ccdc5446e096fb4007368a4f41892a4df4b6
Ahmad Zafrullah <23Pstars@gmail.com>
2022-09-09 17:21:03 +0800

    updates

--- mandiri_fetcher.py
+++ mandiri_fetcher.py
@@ -1,51 +1,82 @@
 #!/usr/bin/python
 
 import os.path
-import urllib
+import urllib.request
 
-src = '/home/zaf/Nextcloud/mandiri/20221/pernyataan/Pembayaran_20220705.csv'
-dst = '/home/zaf/Nextcloud/mandiri/20221/pernyataan/'
+# src = 'O:\\My Drive\\PUSTIK\\Audit\\2022\\14\\Pembayaran_2022_Pil1_Pendidikan_Dokter.csv'
+# src = 'O:\\My Drive\\PUSTIK\\Audit\\2022\\14\\Pembayaran_2022_Pil2_Pendidikan_Dokter.csv'
+# src = 'O:\\My Drive\\PUSTIK\\Audit\\2022\\14\\Pembayaran_2022_Pil1_Farmasi.csv'
+src = 'O:\\My Drive\\PUSTIK\\Audit\\2022\\14\\Pembayaran_2022_Pil2_Farmasi.csv'
+dst = 'O:\\My Drive\\PUSTIK\\Audit\\2022\\14\\docs\\'
+
+# p = '1'
+p = '2'
+
+# p_index = 12
+p_index = 16
 
 with open(src) as _content:
     contents = _content.readlines()
 
 _contents = [i.strip().replace('"', '') for i in contents]
+_contents.pop(0)
 
 for _row in _contents:
-    _row = _row.split(',')
+    _row = _row.split(';')
 
     __no_tes = _row[2]
     if __no_tes == '':
         continue
 
-    __pil1 = _row[12]
-    if __pil1 == '':
+    __pil = _row[p_index]
+    if __pil == '':
         continue
 
-    ___pil1 = __pil1.split('.')
+    ___pil = __pil.split('.')
 
-    if len(___pil1) <= 1:
+    if len(___pil) <= 1:
         continue
 
-    __pil1_dest = dst + __no_tes + '-p1.' + ___pil1[-1]
-    if os.path.exists(__pil1_dest):
+    __pil_dest = dst + __no_tes + '-p'+p+'.' + ___pil[-1]
+    if os.path.exists(__pil_dest):
         continue
 
-    urllib.urlretrieve(__pil1, __pil1_dest)
-    print(__pil1_dest)
+    try:
+        urllib.request.urlretrieve(__pil, __pil_dest)
+        print(__pil_dest)
+    except:
+        print(__pil_dest+' --> file not found!')
 
-    __pil2 = _row[18]
-    if __pil2 == '':
-        continue
 
-    ___pil2 = __pil2.split('.')
 
-    if len(___pil2) <= 1:
-        continue
+    # __pil1 = _row[12]
+    # if __pil1 == '':
+    #     continue
 
-    __pil2_dest = dst + __no_tes + '-p2.' + ___pil2[-1]
-    if os.path.exists(__pil2_dest):
-        continue
+    # ___pil1 = __pil1.split('.')
 
-    urllib.urlretrieve(__pil2, __pil2_dest)
-    print(__pil2_dest)
+    # if len(___pil1) <= 1:
+    #     continue
+
+    # __pil1_dest = dst + __no_tes + '-p1.' + ___pil1[-1]
+    # if os.path.exists(__pil1_dest):
+    #     continue
+
+    # urllib.urlretrieve(__pil1, __pil1_dest)
+    # print(__pil1_dest)
+
+    # __pil2 = _row[18]
+    # if __pil2 == '':
+    #     continue
+
+    # ___pil2 = __pil2.split('.')
+
+    # if len(___pil2) <= 1:
+    #     continue
+
+    # __pil2_dest = dst + __no_tes + '-p2.' + ___pil2[-1]
+    # if os.path.exists(__pil2_dest):
+    #     continue
+
+    # urllib.urlretrieve(__pil2, __pil2_dest)
+    # print(__pil2_dest)
